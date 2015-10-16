6036c9ee39061449e287fdbab7d426c0a3407ca0
Ian Ribas <ian@chaordicsystems.com>
2015-10-16 16:37:50 -0400

    Agora com parametro de linha de comando.

--- extrator.py
+++ extrator.py
@@ -8,6 +8,7 @@ import re
 import urlparse
 import os.path
 import urllib
+import sys
 
 
 pat_d = re.compile("(\d\d)?/?(\d\d)?/?(\d\d\d\d)")
@@ -23,34 +24,39 @@ def format_date(string):
 
     return ret
 
-r = requests.get("http://www.an.gov.br/sian/multinivel/multinivel_consulta5.asp?v_codReferenciaPai_ID=1316324&v_codFundo_ID=&v_nivel=4&v_pula_nivel=")
-s1 = bs4.BeautifulSoup(r.text, "html.parser")
+if __name__ == "__main__":
+    first_url = "http://www.an.gov.br/sian/multinivel/multinivel_consulta5.asp?v_codReferenciaPai_ID=1316324&v_codFundo_ID=&v_nivel=4&v_pula_nivel="
+    if sys.argv[1]:
+        first_url = sys.argv[1]
 
-pat = re.compile('[^\w ]+')
-p_s = re.compile("\s+")
-p_did = re.compile(".*fjs_editar.(\d+).*")
+    r = requests.get(first_url)
+    s1 = bs4.BeautifulSoup(r.text, "html.parser")
 
-doc_links = s1.find_all("a", href=re.compile("javascript:fjs_editar\("))
+    pat = re.compile('[^\w ]+')
+    p_s = re.compile("\s+")
+    p_did = re.compile(".*fjs_editar.(\d+).*")
+
+    doc_links = s1.find_all("a", href=re.compile("javascript:fjs_editar\("))
 
 
-for l in doc_links:
-    n = l.parent.parent.contents[7].text
-    n = format_date(l.parent.parent.contents[11].text) + p_s.sub("_", pat.sub("", n))[:20]
-    did = p_did.sub(r"\1", l["href"])
-    lnk = "http://www.an.gov.br/sian/Multinivel/Lista_Diretorios.asp?visualiza=0&v_CodReferencia_ID=" + did
-    print n, lnk
+    for l in doc_links:
+        n = l.parent.parent.contents[7].text
+        n = format_date(l.parent.parent.contents[11].text) + p_s.sub("_", pat.sub("", n))[:20]
+        did = p_did.sub(r"\1", l["href"])
+        lnk = "http://www.an.gov.br/sian/Multinivel/Lista_Diretorios.asp?visualiza=0&v_CodReferencia_ID=" + did
+        print n, lnk
 
-    r = requests.get(lnk)
-    s2 = bs4.BeautifulSoup(r.text, "html.parser")
-    la = s2.find("a", href=re.compile("Mostra_Arquivo.asp"))
-    if la:
-        v_arquivo = urlparse.parse_qs(urlparse.urlparse(la["href"]).query).get("v_arquivo")[0]
+        r = requests.get(lnk)
+        s2 = bs4.BeautifulSoup(r.text, "html.parser")
+        la = s2.find("a", href=re.compile("Mostra_Arquivo.asp"))
+        if la:
+            v_arquivo = urlparse.parse_qs(urlparse.urlparse(la["href"]).query).get("v_arquivo")[0]
 
-        i = 0
-        fname = n + v_arquivo[-4:]
-        while os.path.isfile(fname) and i < 10000:
-            i += 1;
-            fname = n + "_" + str(i) +  v_arquivo[-4:]
+            i = 0
+            fname = n + v_arquivo[-4:]
+            while os.path.isfile(fname) and i < 10000:
+                i += 1;
+                fname = n + "_" + str(i) +  v_arquivo[-4:]
 
-        urllib.urlretrieve (v_arquivo, fname)
-        print "Saved", v_arquivo, " to ", fname
+            urllib.urlretrieve (v_arquivo, fname)
+            print "Saved", v_arquivo, " to ", fname
